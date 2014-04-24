8e003ca58bcc0a2da105473ac5a483137cc78b48
Shailesh Patil <shailesh@joshsoftware.com>
2014-04-24 10:39:52 +0530

    Changed title & added partners

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,12 +2,12 @@ class ContactsController < ApplicationController
   before_action :load_tweet
 
   def new
-    @title = 'Contact Us | Josh Software Private Limited'
+    @title = 'Contact Us | Synergy Software Solutions Pte Ltd'
     @contact = Contact.new
   end
 
   def create
-    @title = 'Contact Us | Josh Software Private Limited'
+    @title = 'Contact Us | Synergy Software Solutions Pte Ltd'
     @contact = Contact.new params[:contact]
     #@contact.message = @contact.message[0..290]
     if @contact.valid?
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,36 +4,36 @@ class StaticController < ApplicationController
 
   def portfolio
     @projects = PROJECTS.select { |p| p[:display] }
-    @keywords = "Josh Software portfolio, works we have accompolished, #{@projects.collect{|p| p[:title]}.join(", ")}"
+    @keywords = "Synergy Software portfolio, works we have accompolished, #{@projects.collect{|p| p[:title]}.join(", ")}"
     @description = "We work in various industry verticals like Insurance, Manufacturing, Media, eCommerce, Social Sector and Information Technology as long as the work is in Ruby."
-    @title = 'Portfolio | Josh Software Private Limited'
+    @title = 'Portfolio | Synergy Software Solutions Pte Ltd'
   end
 
   def team
     @team_members = TEAM_MEMBERS.sort{|x,y| x[:index] <=> y[:index]}.select { |t| t[:display] }
     team =  @team_members.collect{|t| t[:name]}.join(", ")
-    @keywords = "The JOSH People, the team, #{team}"
-    @description = "The JOSH People, the team behind our success. #{team}"
-    @title = 'The JOSH People | Josh Software Private Limited'
+    @keywords = "The Synergy People, the team, #{team}"
+    @description = "The Synergy People, the team behind our success. #{team}"
+    @title = 'The Synergy People | Synergy Software Solutions Pte Ltd'
   end
 
   def home
     @projects = PROJECTS.select { |p| p[:display] }
-    @title = 'Ruby on Rails experts in India | Josh Software Private Limited'
+    @title = 'Ruby on Rails experts in India | Synergy Software Solutions Pte Ltd'
   end
 
   def testimonials
     @testimonies = TESTIMONIES.select{|e| e[:display]}
     @keywords = "Client testimonials"
     @description = "Nothing certifies our skill other than testimonials, here are the few testimonials from our clients"
-    @title = 'Testimonials | Josh Software Private Limited'
+    @title = 'Testimonials | Synergy Software Solutions Pte Ltd'
   end
 
   def open_source
     @open_sources = OPEN_SOURCE.select{|e| e[:display]}
     @keywords = "Open source Friday, open source contribution, open source involvement"
     @description = "Every Friday evening, we do different things - working on an open-source gem, doing open-source projects, learning new things and having fun."
-    @title = 'Open source contribution and involvement | Josh Software Private Limited'
+    @title = 'Open source contribution and involvement | Synergy Software Solutions Pte Ltd'
   end
 
   def ping
@@ -41,19 +41,19 @@ class StaticController < ApplicationController
   end
 
   def contact_us
-    @title = 'Contact Us | Josh Software Private Limited'
+    @title = 'Contact Us | Synergy Software Solutions Pte Ltd'
   end
   
   def about_us
-    @title = 'About Us | Josh Software Private Limited'
+    @title = 'About Us | Synergy Software Solutions Pte Ltd'
   end
 
   def thankyou
-    @title = 'Thank you | Josh Software Private Limited'
+    @title = 'Thank you | Synergy Software Solutions Pte Ltd'
   end
 
   def news
-    @title = 'Josh in News | Josh Software Private Limited'
+    @title = 'Josh in News | Synergy Software Solutions Pte Ltd'
     @news = NEWS.select{|n| n[:display]}
     @interviews = INTERVIEWS.select{|i| i[:display]}
     @articles = ARTICLES.select{|a| a[:display]}
